0e21434b371e205554c0ff6950394b7a95c16c6d
yuramilkyway <yuramilkyway@gmail.com>
2022-06-30 18:54:15 +0300

    Initial commit

---
+++ src/main/java/com/apress/todo/controller/ToDoController.java
@@ -0,0 +1,26 @@
+package com.apress.todo.controller;
+
+import com.apress.todo.domain.ToDo;
+import com.apress.todo.repository.CommonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ToDoController {
+
+    private CommonRepository<ToDo> repository;
+
+    @Autowired
+    public ToDoController(CommonRepository<ToDo> repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/todo")
+    public ResponseEntity<Iterable<ToDo>> getToDos() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+}
---
+++ src/main/java/com/apress/todo/domain/ToDoBuilder.java
@@ -0,0 +1,33 @@
+package com.apress.todo.domain;
+
+import java.util.Objects;
+
+public class ToDoBuilder {
+
+    private static ToDoBuilder instance = new ToDoBuilder();
+    private String id = null;
+    private String description = "";
+
+    private ToDoBuilder(){}
+
+    public static ToDoBuilder create() {
+        return instance;
+    }
+
+    public ToDoBuilder withDescription(String description) {
+        this.description = description;
+        return instance;
+    }
+
+    public ToDoBuilder withId(String id) {
+        this.id = id;
+        return instance;
+    }
+
+    public ToDo build() {
+        ToDo result = new ToDo(this.description);
+        if (Objects.nonNull(id))
+            result.setId(id);
+        return result;
+    }
+}
